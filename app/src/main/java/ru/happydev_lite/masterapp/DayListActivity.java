75602a21c41bdcc6bb828bfa932211a12012c62e
Alex Korovyansky <alex@medlert.com>
2014-09-14 13:17:50 +0700

    Pomodoro #4

--- app/src/main/java/ru/happydev_lite/masterapp/DayListActivity.java
+++ app/src/main/java/ru/happydev_lite/masterapp/DayListActivity.java
@@ -1,22 +1,89 @@
 package ru.happydev_lite.masterapp;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ViewAnimator;
+
+import java.util.List;
 
 
 public class DayListActivity extends ListActivity {
 
+    public class RefreshTask extends AsyncTask<Void, Void, List<Forecast>> {
+
+        @Override
+        protected List<Forecast> doInBackground(Void... params) {
+            SystemClock.sleep(3000);
+            return Forecast.makeRandom(7);
+        }
+
+        @Override
+        protected void onPostExecute(List<Forecast> forecasts) {
+            super.onPostExecute(forecasts);
+            if (isCancelled()) {
+                return;
+            }
+            ArrayAdapter<Forecast> arrayAdapter = new ArrayAdapter<Forecast>(
+                    DayListActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1,
+                    forecasts
+            );
+            setListAdapter(arrayAdapter);
+            viewAnimator.setDisplayedChild(0);
+        }
+    }
+
+    private ViewAnimator viewAnimator;
+    private RefreshTask refreshTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayAdapter<Forecast> arrayAdapter = new ArrayAdapter<Forecast>(
-                this, android.R.layout.simple_list_item_1, android.R.id.text1,
-                Forecast.makeRandom(7)
-        );
-        setListAdapter(arrayAdapter);
+        setContentView(R.layout.activity_days_list);
+        viewAnimator = (ViewAnimator) findViewById(R.id.day_list_animator);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.daily, menu);
+        return true;
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (refreshTask != null) {
+            refreshTask.cancel(true);
+        }
+    }
+
+    private void refresh() {
+        if (refreshTask != null && !refreshTask.isCancelled()) {
+            refreshTask.cancel(true);
+        }
+        refreshTask = new RefreshTask();
+        refreshTask.execute();
+        viewAnimator.setDisplayedChild(1);
+    }
 }
